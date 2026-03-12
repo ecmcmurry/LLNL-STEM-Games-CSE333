6e7444f6477d48ab3c0953d849da67734046937c
Elizabeth McMurry <nmcmurry@ucmerced.edu>
2026-03-12 11:01:01 -0700

    Added cell selection for cells within the main Grid

--- Elizabeth Games/gears/cell.js
+++ Elizabeth Games/gears/cell.js
@@ -1,38 +1,42 @@
 //Cell needs to define the basic components of a cell
 
-const size = 80;
 const lineWidth = 5;
 
 export class Cell {
     //Expand to include more information like contents
     //Maybe change names to be like, rowIndex?
-    constructor(row, col, isBlocked = false) {
+    constructor(row, col, size = 80, isBlocked = false) {
         this.row = row;
         this.col = col;
+        this.size = size;
         //I might change this into a component
         this.isBlocked = isBlocked;
+        this.isSelected = false;
     }
 
     draw(ctx) {
         //This shifts each cell over by the length of a cell. Without this they would all be on top of each other
-        let x = this.col * size;
-        let y = this.row * size;
+        //They are further shifted by the width of their border, to prevent any overlap
+        //And finally have an offset of half their width to prevent the grid from getting cutoff from the canvas
+        let x = (this.col * (this.size + lineWidth)) + (lineWidth/2);
+        let y = (this.row * (this.size + lineWidth)) + (lineWidth/2);
 
         //Draws the rectangle for the cell
         ctx.lineWidth = lineWidth;
 
-        //This offsets the first rows and columns just a little bit to prevent them from getting clipped by the canvas
-        if (this.row == 0) {
-            y = y + (lineWidth/2);
-        }
-        if (this.col == 0) {
-            x = x + (lineWidth/2);
+        //If the cell is selected, it should be drawn in a different color
+        if (this.isSelected == true) {
+            ctx.strokeStyle = "rgb(255, 0, 0)";
+        } else {
+            ctx.strokeStyle = "rgb(0, 0, 0)";
         }
 
+        //if a cell is blocked, draw it as a solid rectangle
         if (!this.isBlocked) {
-            ctx.strokeRect(x, y, size, size);
+            ctx.strokeRect(x, y, this.size, this.size);
+        //otherwise, draw a hollow rectangle
         } else {
-            ctx.fillRect(x, y, size, size);
+            ctx.fillRect(x, y, this.size, this.size);
         }
         //This should also call the draw for the components when they are implemented.
     }
--- Elizabeth Games/gears/grid.js
+++ Elizabeth Games/gears/grid.js
@@ -2,11 +2,14 @@
 
 import { Cell } from "./cell.js";
 
+//let selectedCell = null;
+
 export class Grid {
     //maybe update to be rowNum or rowCount?
-    constructor(rows, cols) {
+    constructor(rows, cols, size = 80) {
         this.rows = rows;
         this.cols = cols;
+        this.size = size;
 
         //need some way for the game to pass in a defined set of cells
         //such as loading from a string
@@ -20,7 +23,11 @@ export class Grid {
             //Adds a new cell to the current row for each column
             //The rowIndex and columnIndex information is passed into cells as they are created
             for (let columnIndex = 0; columnIndex < cols; columnIndex++) {
-                currentRow.push(new Cell(rowIndex, columnIndex));
+                if (columnIndex == 1 && rowIndex == 1) {
+                    currentRow.push(new Cell(rowIndex, columnIndex, size, true));
+                    continue;
+                }
+                currentRow.push(new Cell(rowIndex, columnIndex, size));
             }
 
             //After the row list is filled, it is pushed into the cells list
@@ -37,5 +44,41 @@ export class Grid {
                 this.cells[r][c].draw(ctx);
             }
         }
+    }
+
+    handleClick(x,y) {
+        //I had to update the grid and cells so that the grid can now define the size of the cells
+        //This is important because it means that I can have the decoding from screen/canvas coord -> row/col here, so that the cell only has to worry about it's actual logic
+        
+        let offsetSize = this.size + 5;
+
+        if (x > offsetSize*this.rows) {
+            return;
+        }
+        if (y > offsetSize*this.cols) {
+            return;
+        }
+
+        //This is currently hard-coded to use the line width of 5 that is used for drawing cells
+        //TODO: Update to be more dynamic
+        let selCol = Math.floor((x-2.5) / offsetSize);
+        let selRow = Math.floor((y-2.5) / offsetSize);
+
+        // //Checks if there is a cell that was previously selected before selecting a new cell
+        // if (selectedCell != null && selectedCell.isSelected == true) {
+        //     //if so, unselect that cell
+        //     selectedCell.isSelected = false;
+        //     selectedCell = null;
+        // }
+
+        if (this.selectedCell && this.selectedCell.isSelected) {
+            this.selectedCell.isSelected = false;
+        }
+
+        //We use our newly translated coordinates to identify the proper cell
+        this.selectedCell = this.cells[selRow][selCol]
+        //Then we call the select function of that cell
+        this.selectedCell.isSelected = true;
+        console.log("selected a cell");
     }
 }
