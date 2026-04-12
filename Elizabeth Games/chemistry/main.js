d1f579aacb2d7d78fa79b9787d6a17464977df4a
Elizabeth McMurry <nmcmurry@ucmerced.edu>
2026-04-12 14:19:01 -0700

    Added funtional initial draft of Disposal screen

--- Elizabeth Games/chemistry/main.js
+++ Elizabeth Games/chemistry/main.js
@@ -4,7 +4,18 @@ const predictionCards = document.getElementById('predictionCards');
 let reaction = null;
 let predictions = null;
 
-//Changes from the Title Screen to the Predictions Screen
+//Changes from the Title Screen to the Pre-Lab Screen
+//This is included in main and NOT swapScreen because of the added functionality when you press the button
+document.getElementById('titleToPreLabBtn').addEventListener('click', () => {
+    swapScreen("prelab")
+
+    //pulls the value from the reaction dropdown and stores that as the current reaction
+    reaction = reactionDropdown.value;
+
+    document.getElementById('preLabBriefing').innerHTML = REACTIONS[reaction].prelab;
+});
+
+//Changes from the Pre-Lab to the Predictions Screen
 document.getElementById('preLabToPredictionsBtn').addEventListener('click', () => {
     swapScreen("prediction");
     //This is essentially the first-time-setup for the prediction screen. Maybe move to a specific file for screen management if this gets out of hand
@@ -48,6 +59,9 @@ document.getElementById('preLabToPredictionsBtn').addEventListener('click', () =
             optionButton.name = `option${cardIndex}`;
             optionButton.value = option;
             optionButton.id = `card${cardIndex}_option${optionIndex}`;
+            if (optionIndex === 0) {
+                optionButton.checked = true;
+            }
 
             //Create the label and fill it with the text of the answer
             let label = document.createElement('label');
@@ -71,13 +85,76 @@ document.getElementById('preLabToPredictionsBtn').addEventListener('click', () =
     });
 });
 
-//I feel like there has to be a better way of doing this. Maybe I can call the functions directly within index.html?
-//Changes from the Title Screen to the Pre-Lab Screen
-document.getElementById('titleToPreLabBtn').addEventListener('click', () => {
-    swapScreen("prelab")
+//Verifies that the PPE options chosen by the player line up with the reaction
+document.getElementById('verifyPPE').addEventListener('click', () => {
+    //pulls answer from each category
+    //checks that against value in reaction
+    let correctSum = 0;
 
-    //pulls the value from the reaction dropdown and stores that as the current reaction
-    reaction = reactionDropdown.value;
+    const eye = document.querySelector('input[name="eyeAndFaceProtection"]:checked');
+    const hands = document.querySelector('input[name="handProtection"]:checked');
+    const body = document.querySelector('input[name="bodyProtection"]:checked');
+    const foot = document.querySelector('input[name="footProtection"]:checked');
+    const respiratory = document.querySelector('input[name="respiratoryProtection"]:checked');
 
-    document.getElementById('preLabBriefing').innerHTML = REACTIONS[reaction].prelab;
+    //there is almost certainly a smarter way of doing this
+    if (eye.value == REACTIONS[reaction].safety.eyeAndFace) {
+        correctSum++;
+    }
+    if (hands.value == REACTIONS[reaction].safety.hands) {
+        correctSum++;
+    }
+    if (body.value == REACTIONS[reaction].safety.body) {
+        correctSum++;
+    }
+    if (foot.value == REACTIONS[reaction].safety.foot) {
+        correctSum++;
+    }
+    if (respiratory.value == REACTIONS[reaction].safety.respiratory) {
+        correctSum++;
+    }
+
+    if (correctSum == 5) {
+        //if correct display this
+        document.getElementById('PPEFeedback').innerText = "Good job!";
+        document.getElementById('safetyScreen').querySelector('.toLabBtn').classList.remove('hidden');
+    } else {
+        //if incorrect display this
+        document.getElementById('PPEFeedback').innerText = "Include an API call here so the AI can guide you to the right answers? Anyways, you got it wrong somehow";
+        //If, for some reason, the user goes back to the safety screen and makes an incorrect guess, we want the button to be hidden again
+        document.getElementById('safetyScreen').querySelector('.toLabBtn').classList.add('hidden');
+    }
+});
+
+//Verifies that the Disposal options chosen by the player line up with the reaction
+document.getElementById('verifyDisposal').addEventListener('click', () => {
+    //pulls answer from each category
+    //checks that against value in reaction
+    let correctSum = 0;
+
+    const liquid = document.querySelector('input[name="liquidWaste"]:checked');
+    const solid = document.querySelector('input[name="solidWaste"]:checked');
+    const gas = document.querySelector('input[name="gaseousWaste"]:checked');
+
+    //there is almost certainly a smarter way of doing this
+    if (liquid.value == REACTIONS[reaction].disposal.liquid) {
+        correctSum++;
+    }
+    if (solid.value == REACTIONS[reaction].disposal.solid) {
+        correctSum++;
+    }
+    if (gas.value == REACTIONS[reaction].disposal.gaseous) {
+        correctSum++;
+    }
+
+    if (correctSum == 3) {
+        //if correct display this
+        document.getElementById('disposalFeedback').innerText = "Good job!";
+        document.getElementById('disposalScreen').querySelector('.toLabBtn').classList.remove('hidden');
+    } else {
+        //if incorrect display this
+        document.getElementById('disposalFeedback').innerText = "Include an API call here so the AI can guide you to the right answers? Anyways, you got it wrong somehow";
+        //If, for some reason, the user goes back to the disposal screen and makes an incorrect guess, we want the button to be hidden again
+        document.getElementById('disposalScreen').querySelector('.toLabBtn').classList.add('hidden');
+    }
 });
