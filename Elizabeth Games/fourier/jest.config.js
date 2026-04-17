be530830709bcec7cd569f02b093bad9b371e818
e-mcmurry <nmcmurry@ucmerced.edu>
2026-04-17 12:50:58 -0700

    Revert "Added Claude generated files to support Jest unit tests and Playwright E2E tests"
    This reverts commit 6530a32cd4ae93e418978797d973a0a30073293e.

--- Elizabeth Games/fourier/jest.config.js
+++
@@ -1,8 +0,0 @@
-// jest.config.js
-module.exports = {
-  testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.unit.test.js'],
-  collectCoverageFrom: ['src/**/*.js'],
-  coverageReporters: ['text', 'lcov'],
-  verbose: true,
-};
